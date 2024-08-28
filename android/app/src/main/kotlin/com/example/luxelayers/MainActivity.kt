c5723060901fc19b4c8c763d1a2cad65bc1ac863
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-08-28 12:34:50 +0530

    Mobile App first page created

---
+++ android/app/src/main/kotlin/com/example/luxelayers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.luxelayers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:luxelayers/Login%20and%20Signup%20Page/loginpage.dart';
+import 'package:luxelayers/firebase_options.dart';
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    FirebaseAuth _auth=FirebaseAuth.instance;
+    final user=_auth.currentUser;
+    return  const MaterialApp(
+      title: "LuxeLayers",
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+
+    );
+  }
+}
+
