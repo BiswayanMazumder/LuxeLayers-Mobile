eae7821e6bcfd8f88b54d230c688dd63cb99f34e
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-08-29 19:55:53 +0530

    Home Page creation #1

--- lib/Logged In Page/homepage.dart
+++ lib/Logged In Page/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,26 +11,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List highlightimages = [
-    'https://pbs.twimg.com/media/FyDbitjWAAEhh_P.jpg:large',
-    'https://marketplace.mainstreet.co.in/cdn/shop/files/v3.webp?v=1724767282&width=823',
-    'https://static.nike.com/a/images/w_960,c_limit/6a90b46c-87bb-4882-bce0-f5b856019b78/image.png',
+  final List<String> imageUrls = [
     'https://images.vegnonveg.com/media/collections/102/172007371710266863df57389e.png',
     'https://images.vegnonveg.com/media/collections/75/171955723875667e5c76f082e.png',
     'https://images.vegnonveg.com/media/collections/101/17198391211016682a991ee9b7.png',
+    // Add more image URLs here
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Icon(
-          Icons.menu,
-          color: Colors.black,
-        ),
+        automaticallyImplyLeading: false,
         title: Text(
-          'Shop',
+          'Discover',
           style:
-              GoogleFonts.nunitoSans(fontWeight: FontWeight.w500, fontSize: 18),
+              GoogleFonts.nunitoSans(fontWeight: FontWeight.w700, fontSize: 20),
         ),
         actions: [
           Padding(
@@ -51,26 +48,74 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const SizedBox(
-              height: 30,
+            const SizedBox(height: 30),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  border: Border.all(color: Colors.black),
+                ),
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: 50,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal:
+                          10.0), // Add padding to prevent the TextField from touching the container's border
+                  child: TextField(
+                    style: GoogleFonts.nunitoSans(),
+                    decoration: const InputDecoration(
+                      suffixIcon: Icon(Icons.search, color: Colors.black),
+                      hintText: 'Search',
+                      border: InputBorder.none, // Remove the default border
+                    ),
+                  ),
+                ),
+              ),
             ),
+            const SizedBox(height: 30),
+            SizedBox(
+              height: 200, // Fixed height for the PageView
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: PageView.builder(
+                  itemCount: imageUrls.length,
+                  itemBuilder: (context, index) {
+                    return Image.network(
+                      imageUrls[index],
+                      fit: BoxFit.fill,
+                    );
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
             Padding(
-              padding: const EdgeInsets.only(left: 20),
+              padding: const EdgeInsets.all(20.0),
               child: Row(
                 children: [
                   Text(
-                    "This Week's Highlights",
+                    'Categories',
                     style: GoogleFonts.nunitoSans(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 20),
-                  )
+                        fontWeight: FontWeight.w700, fontSize: 20),
+                  ),
+                  const Spacer(),
+                  InkWell(
+                    onTap: (){
+                      if (kDebugMode) {
+                        print('CLicked');
+                      }
+                    },
+                    child: Text(
+                      'See All',
+                      style: GoogleFonts.nunitoSans(
+                        color: Colors.green,
+                          fontWeight: FontWeight.w700, fontSize: 15),
+                    ),
+                  ),
                 ],
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
+            )
           ],
         ),
       ),
