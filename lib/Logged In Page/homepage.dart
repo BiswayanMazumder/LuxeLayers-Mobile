c1b5cad5a1e29731b9a8256b1e5ad93d0c944a45
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-08-30 11:56:18 +0530

    Home Page creation #5 =>Categories working

--- lib/Logged In Page/homepage.dart
+++ lib/Logged In Page/homepage.dart
@@ -18,7 +18,7 @@ class _HomePageState extends State<HomePage> {
     // Add more image URLs here
   ];
   List<String> categories = ['All', 'Air Jordan', 'Slides', 'Dunk'];
-  List<String> sneakerimages=[
+  List<String> sneakerimages = [
     "https://images.vegnonveg.com/resized/400X328/11473/air-max-90-drift-action-greenblack-summit-white-green-66c6f5a6b95b7.jpg",
     "https://images.vegnonveg.com/resized/400X328/11421/air-max-1-whitelight-army-neutral-grey-black-white-66b47248b5e8d.jpg",
     "https://images.vegnonveg.com/resized/400X328/11421/air-max-1-whitelight-army-neutral-grey-black-white-66b4724907e18.jpg",
@@ -70,7 +70,7 @@ class _HomePageState extends State<HomePage> {
     "https://images.vegnonveg.com/resized/400X328/11436/chuck-70-mule-slip-egretblack-black-66c6d00abb1b9.jpg",
     "https://images.vegnonveg.com/resized/400X328/11439/run-star-utility-sandal-cx-ox-egret-cream-66c6d2efece41.jpg"
         // "https://images.vegnonveg.com/media/collections/84/171955716484667e5c2c97284.png",
-    "https://images.vegnonveg.com/resized/400X328/11317/air-force-1-shadow-sailcacao-wow-flax-sesame-white-6690be258346c.jpg",
+        "https://images.vegnonveg.com/resized/400X328/11317/air-force-1-shadow-sailcacao-wow-flax-sesame-white-6690be258346c.jpg",
     "https://images.vegnonveg.com/resized/400X328/11226/air-force-1-07-lv8-light-british-tanburgundy-crush-brown-666a86b38436d.jpg",
     "https://images.vegnonveg.com/resized/400X328/11336/air-force-1-07-lv8-1-coconut-milkvintage-green-bicoastal-white-6690cdf402403.jpg",
     "https://images.vegnonveg.com/resized/400X328/11257/air-force-1-07-nn-hydrangeasblack-raspberry-barely-grape-purple-66865dbe70bcc.jpg",
@@ -80,19 +80,81 @@ class _HomePageState extends State<HomePage> {
     "https://images.vegnonveg.com/resized/400X328/11475/air-force-1-07-whitedragon-red-white-white-66c717cce8961.jpg"
   ];
   int _selectedIndex = 0; // Track the selected index
-  List<String> sneakernames=[
-    "AIR JORDAN 1 LOW 'OFF NOIR/ARCHAEO BROWN-SAIL'",
-    "AIR JORDAN 1 LOW 'WHITE/METALLIC GOLD-BLACK'",
-    "AIR JORDAN 1 LOW SE 'OXIDIZED GREEN/WHITE-SAIL'",
-    "DUNK LOW RETRO 'WHITE/GREY FOG'",
-    "DUNK LOW NN 'SUMMIT WHITE/KHAKI-BAROQUE BROWN-PHANTOM'",
-    "DUNK LOW RETRO 'WHITE/DRAGON RED-BLACK'",
-    "JORDAN JUMPMAN SLIDE NEUTRAL GREY/METALLIC SILVER",
-    "AIR FORCE 1 SHADOW 'SAIL/CACOA WOW-FLAX-SESAME WHITE'",
-    "AIR FORCE 1 '07 NN 'HYDRANGEAS/BLACK-RASPBERRY-BARELY GRAPE PURPLE'",
-    "AIR MAX 1 'WHITE/LIGHT ARMY-NEUTRAL GREY-BLACK'"
-    "AIR MAX 90 DRIFT 'ACTION GREEN/BLACK-SUMMIT WHITE'",
-    "AIR MAX 1 ESSENTIAL 'WHITE/BLACK-SUMMIT WHITE'"
+  List <String> jordans=[
+    "https://images.vegnonveg.com/resized/400X328/11393/air-jordan-1-low-off-noirarchaeo-brown-sail-brown-66b362b30f4dc.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11393/air-jordan-1-low-off-noirarchaeo-brown-sail-brown-66b362b35ce83.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11422/air-jordan-4-retro-se-smoke-greyiron-grey-cement-grey-grey-66b48d859813d.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11422/air-jordan-4-retro-se-smoke-greyiron-grey-cement-grey-grey-66b48d8603d63.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11425/air-jordan-1-low-whitemetallic-gold-black-white-66bb4992d8cb4.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11425/air-jordan-1-low-whitemetallic-gold-black-white-66bb499322a13.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11381/air-jordan-1-mid-se-whiteoxidized-green-sail-neutral-grey-white-66a8c651cc6f3.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11247/air-jordan-1-low-se-oxidized-greenwhite-sail-green-66a8c54429f5d.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11247/air-jordan-1-low-se-oxidized-greenwhite-sail-green-66a8c544725c4.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11241/air-jordan-1-low-se-hemplight-british-tan-sail-oatmeal-brown-667d3e4f7c748.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11241/air-jordan-1-low-se-hemplight-british-tan-sail-oatmeal-brown-667d3e4fd25f1.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11429/air-jordan-4-retro-blackwhite-black_1-66c71921807e0.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11429/air-jordan-4-retro-blackwhite-black_1-66c71921c754a.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11428/air-jordan-4-retro-blackwhite-black-66c718d11e771.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11426/air-jordan-1-mid-saillight-dew-muslin-white-66bb4a42e2b7a.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11426/air-jordan-1-mid-saillight-dew-muslin-white-66bb4a431e495.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11216/air-jordan-11-retro-low-whitemidnight-navy-diffused-blue-white-6662eaf4ed74c.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11216/air-jordan-11-retro-low-whitemidnight-navy-diffused-blue-white-6662eaf5294de.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11395/air-jordan-5-retro-whiteblack-sail-metallic-silver-white-66b365aa44059.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11394/air-jordan-1-low-whitebordeaux-sail-purple-66b364fe14e20.jpg",
+    "https://images.vegnonveg.com/resized/400X328/9805/air-jordan-1-mid-whiteblack-white-64dcc2a91c4af.jpg",
+    "https://images.vegnonveg.com/resized/400X328/9805/air-jordan-1-mid-whiteblack-white-64dcc2a979f95.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11375/air-jordan-1-retro-high-og-blackmetallic-gold-sail-black_1-66a8c4bbb4277.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11375/air-jordan-1-retro-high-og-blackmetallic-gold-sail-black_1-66a8c4bc2d73f.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11374/air-jordan-1-retro-high-og-blackmetallic-gold-sail-black-66a8c41a8efe9.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11324/air-jordan-1-retro-high-og-university-blueuniversity-gold-sail-blue-6690d2df36ed7.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11309/air-jordan-1-low-sailneutral-grey-coconut-milk-grey-6690bb424cd21.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11316/air-jordan-1-mid-blackmetallic-gold-white-black-6690bdacce982.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11316/air-jordan-1-mid-blackmetallic-gold-white-black-6690bdad1b6da.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11306/air-jordan-1-low-whitesky-grey-football-grey-white-6690ba64af00f.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11303/air-jordan-1-mid-legend-light-brownsail-muslin-white-6690b3636add7.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11352/air-jordan-3-retro-tex-dark-driftwoodsail-hemp-velvet-brown-brown-6690d2874de39.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11352/air-jordan-3-retro-tex-dark-driftwoodsail-hemp-velvet-brown-brown-6690d28783e41.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11307/air-jordan-1-retro-low-og-whiteblack-wolf-grey-white-6690d206a3124.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11307/air-jordan-1-retro-low-og-whiteblack-wolf-grey-white-6690d206dfa2c.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11315/air-jordan-1-mid-blackwhite-gym-red-black_1-669113064f7db.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11301/air-jordan-1-low-whitewolf-grey-midnight-navy-grey-6690b22d90729.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11344/air-jordan-legacy-312-low-whitelight-dew-sail-white-669113bfa0306.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11248/air-jordan-1-mid-se-neutral-greysmoke-grey-sail-grey-66865b3831d31.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11248/air-jordan-1-mid-se-neutral-greysmoke-grey-sail-grey-66865b386d852.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11233/air-jordan-13-retro-dune-redterra-blush-white-red-667549df633ec.jpg",
+    "https://images.vegnonveg.com/resized/400X328/10654/air-jordan-1-low-85-whitenavy-blue-65ba3f129749e.jpg"
+  ];
+  List <String> slides=[
+    "https://images.vegnonveg.com/resized/400X328/11240/jordan-jumpman-slide-neutral-greymetallic-silver-grey-667d41362b384.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11225/calm-sandal-light-bone-cream-666a83e84e6a9.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11225/calm-sandal-light-bone-cream-666a83e87116a.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11244/calm-slide-se-glacier-blue-blue-66865e0f385b4.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11244/calm-slide-se-glacier-blue-blue-66865e0f57cce.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11245/icon-classic-sandal-blackwhite-black-66865eb998bb6.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11191/calm-slide-game-royal-blue-66508c815106c.jpg",
+    "https://images.vegnonveg.com/resized/400X328/10916/jordan-super-play-slide-blackphantom-anthracite-black-660ff53055b50.jpg",
+    "https://images.vegnonveg.com/resized/400X328/10916/jordan-super-play-slide-blackphantom-anthracite-black-660ff5308138c.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11060/calm-sandal-black-black_1-6634824205ed5.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11060/calm-sandal-black-black_1-663482421f859.jpg",
+    "https://images.vegnonveg.com/resized/400X328/10901/air-max-1-slide-whiteroyal-blue-black-light-neutral-grey-blue-660e942f103d7.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11474/calm-slide-flat-pewter-grey-66c71665c9e1c.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11436/chuck-70-mule-slip-egretblack-black-66c6d00abb1b9.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11439/run-star-utility-sandal-cx-ox-egret-cream-66c6d2efece41.jpg"
+  ];
+  List<String> dunks=[
+    "https://images.vegnonveg.com/resized/400X328/5358/nike-dunk-low-retro-whiteblack-white-60e41a479d3e7.jpg",
+    "https://images.vegnonveg.com/resized/400X328/5363/w-nike-dunk-low-whiteblack-white-60e41b4c68977.jpg",
+    "https://images.vegnonveg.com/resized/400X328/8828/nike-dunk-low-retro-whitegrey-fog-63ce764079383.jpg",
+    "https://images.vegnonveg.com/resized/400X328/8828/nike-dunk-low-retro-whitegrey-fog-63ce764031209.jpg",
+    "https://images.vegnonveg.com/resized/400X328/10539/dunk-low-dark-currywhite-white-658e94dce57fc.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11342/dunk-low-nn-summit-whitekhaki-baroque-brown-phantom-white-6690cf8f03a69.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11434/dunk-low-nn-baroque-brownblack-white-sail-brown-66bb4b2dc3a2a.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11407/dunk-low-blackmidnight-navy-white-university-red-white-66bb4bbad6fab.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11396/dunk-low-retro-whitedenim-turq-white-66b3660dc7ed0.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11406/dunk-low-coconut-milkflax-sail-brown-66b497326da01.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11408/dunk-low-game-royalblack-white-multicolor-66b4784782d8e.jpg",
+    "https://images.vegnonveg.com/resized/400X328/11397/dunk-low-retro-whitedragon-red-black-white-66b3667e7b69f.jpg"
+
   ];
   @override
   Widget build(BuildContext context) {
@@ -240,7 +302,7 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            Padding(
+          _selectedIndex==0?  Padding(
               padding: const EdgeInsets.all(10),
               child: SizedBox(
                 height: 300, // Set a specific height if needed
@@ -261,8 +323,74 @@ class _HomePageState extends State<HomePage> {
                   },
                 ),
               ),
+            ):
+          _selectedIndex==1?
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: SizedBox(
+              height: 300, // Set a specific height if needed
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10.0, // Add spacing between items
+                  mainAxisSpacing: 10.0, // Add spacing between items
+                ),
+                itemCount: jordans.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Image.network(
+                      jordans[index],
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
+              ),
             ),
-
+          ):
+          _selectedIndex==2?
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: SizedBox(
+              height: 300, // Set a specific height if needed
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10.0, // Add spacing between items
+                  mainAxisSpacing: 10.0, // Add spacing between items
+                ),
+                itemCount: slides.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Image.network(
+                      slides[index],
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ):Padding(
+            padding: const EdgeInsets.all(10),
+            child: SizedBox(
+              height: 300, // Set a specific height if needed
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10.0, // Add spacing between items
+                  mainAxisSpacing: 10.0, // Add spacing between items
+                ),
+                itemCount: dunks.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Image.network(
+                      dunks[index],
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
           ],
         ),
       ),
